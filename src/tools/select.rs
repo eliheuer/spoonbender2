082d00d21b8c47c0cd0a70641f5f39d8fcc15bac
Eli H <elih@bezy.org>
2025-11-14 10:43:23 -0800

    fix selection UX issues

--- src/tools/select.rs
+++ src/tools/select.rs
@@ -41,20 +41,10 @@ impl Tool for SelectTool {
 impl MouseDelegate for SelectTool {
     type Data = EditSession;
 
-    fn left_down(&mut self, event: MouseEvent, _data: &mut EditSession) {
-        // TODO: Hit test for points under cursor
+    fn left_down(&mut self, event: MouseEvent, data: &mut EditSession) {
         println!("SelectTool::left_down pos={:?} shift={}", event.pos, event.mods.shift);
-    }
 
-    fn left_up(&mut self, event: MouseEvent, _data: &mut EditSession) {
-        // TODO: Finalize selection
-        println!("SelectTool::left_up pos={:?} shift={}", event.pos, event.mods.shift);
-    }
-
-    fn left_click(&mut self, event: MouseEvent, data: &mut EditSession) {
-        println!("SelectTool::left_click called! pos={:?} shift={} current_selection={}", event.pos, event.mods.shift, data.selection.len());
-
-        // Hit test for a point at the cursor
+        // Hit test for a point at the cursor - selection happens HERE, on mouse down
         if let Some(hit) = data.hit_test_point(event.pos, None) {
             println!("Hit point: {:?} distance={}", hit.entity, hit.distance);
             if event.mods.shift {
@@ -71,11 +61,14 @@ impl MouseDelegate for SelectTool {
                 data.selection = new_selection;
                 data.update_coord_selection();
             } else {
-                // Normal click: replace selection
-                let mut new_selection = crate::selection::Selection::new();
-                new_selection.insert(hit.entity);
-                data.selection = new_selection;
-                data.update_coord_selection();
+                // Normal click: replace selection with just this point
+                // UNLESS the point is already selected (then keep current selection for dragging)
+                if !data.selection.contains(&hit.entity) {
+                    let mut new_selection = crate::selection::Selection::new();
+                    new_selection.insert(hit.entity);
+                    data.selection = new_selection;
+                    data.update_coord_selection();
+                }
             }
         } else {
             if !event.mods.shift {
@@ -87,16 +80,30 @@ impl MouseDelegate for SelectTool {
         }
     }
 
+    fn left_up(&mut self, _event: MouseEvent, _data: &mut EditSession) {
+        // Selection already happened in left_down, nothing to do here
+    }
+
+    fn left_click(&mut self, _event: MouseEvent, _data: &mut EditSession) {
+        // Click is now handled entirely by left_down
+        // This method is called after left_up if no drag occurred
+        // But we don't need to do anything here since selection already happened
+    }
+
     fn left_drag_began(&mut self, event: MouseEvent, _drag: Drag, data: &mut EditSession) {
-        // Check if we're clicking on a selected point
-        if let Some(hit) = data.hit_test_point(event.pos, None) {
-            if data.selection.contains(&hit.entity) {
-                // We're dragging a selected point
-                self.dragging_points = true;
-                // Store the starting position in design space
-                self.last_drag_pos = Some(data.viewport.from_screen(event.pos));
-                println!("Select tool: started dragging {} selected point(s)", data.selection.len());
-                return;
+        // Check if we have any selected points
+        // (They were already selected in left_down)
+        if !data.selection.is_empty() {
+            // Check if we're starting the drag on a selected point
+            if let Some(hit) = data.hit_test_point(event.pos, None) {
+                if data.selection.contains(&hit.entity) {
+                    // We're dragging a selected point
+                    self.dragging_points = true;
+                    // Store the starting position in design space
+                    self.last_drag_pos = Some(data.viewport.from_screen(event.pos));
+                    println!("Select tool: started dragging {} selected point(s)", data.selection.len());
+                    return;
+                }
             }
         }
 
